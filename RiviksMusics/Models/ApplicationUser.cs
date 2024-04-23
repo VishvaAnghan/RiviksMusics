53ceabe9470eb6f679b90ad152faa80744b76482
Ritika Bharodiya <ritikabharodiya@gmail.com>
2024-04-23 17:54:42 +0530

    commit 8

--- RiviksMusics/Models/ApplicationUser.cs
+++ RiviksMusics/Models/ApplicationUser.cs
@@ -5,11 +5,15 @@ namespace RiviksMusics.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Display(Name = "First Name")]
         public string? FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string? LastName { get; set; }
         [MaxLength(10)]
+        [Display(Name = "Phone No")]
         public string? PhoneNo { get; set; }
         public string? Address { get; set; }
+        [Display(Name = "Birth Date")]
         public DateTime BirthDate { get; set; }
         public string? Gender { get; set; }
         public string? Image { get; set; }
